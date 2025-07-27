b08e56c1ca17b61d04a154436e44c7527c20017a
CodingWithSaksham <sakshamthecoder@gmail.com>
2025-07-27 20:26:58 +0530

    Fixed typo

--- hyparse/skills/Dungeons/__init__.py
+++ hyparse/skills/Dungeons/__init__.py
@@ -1,5 +1,4 @@
 from .Dungeons import Dungeons
 from .Catacombs import Catacombs
-from .Master_Catacombs import Master_Catacombs
 
-__all__ = ["Dungeons", "Catacombs", "Master_Catacombs"]
+__all__ = ["Dungeons", "Catacombs"]
